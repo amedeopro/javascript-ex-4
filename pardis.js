ad4bd7a73bb56e363517352f3c36802ccd9131c4
Pro Amedeo <proamedeo@MacBook-Pro-di-Pro.local>
2018-11-19 17:16:49 +0100

    esercizio commentato

---
+++ pardis.js
@@ -0,0 +1,12 @@
+// L’utente sceglie pari o dispari e un numero da
+// 1 a 5. Generiamo un numero random (sempre
+// da 1 a 5) per il computer e dichiariamo chi ha
+// vinto.
+
+//genero un numero da tra 1 e 5 in una funzione
+
+// l'utente sceglie se è pari o dispari
+
+// una volta che l'utente ha scelto la cpu sarà di conseguenza pari o dispari
+
+//creo la condizione in cui se un numero è pari o dispari e decreto il vincitore
