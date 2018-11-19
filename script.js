ff45c0a95b33e94f9358c95e55d6a70a86a26874
Pro Amedeo <proamedeo@MacBook-Pro-di-Pro.local>
2018-11-19 17:10:07 +0100

    palindromo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert('OK!')
+// Una funziona per capire se la parola è
+// palindroma
+
+
+
+var parola = prompt("Inserisci una parola");
+
+function checkPalindrom(str) {
+    return str == str.split('').reverse().join('');
+}
+
+if (checkPalindrom(parola)) {
+  document.writeln('la parola è palindroma');
+} else {
+  document.writeln('la parola NON è palindroma');
+}
